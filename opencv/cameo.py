667ba7c686125a1569f69773058482248ea4ccee
Reyes Chen <adolphgirl@gmail.com>
2020-10-31 15:16:15 +0800

    opencv 3 init

---
+++ opencv/cameo.py
@@ -0,0 +1,60 @@
+# -*- coding: utf-8 -*-
+import cv2
+import datetime
+from managers import WindowManager, CaptureManager
+
+
+class Cameo(object):
+    def __init__(self):
+        # 建立一個視窗，並將鍵盤的回撥函式傳入
+        self._windowManager = WindowManager('Video_Date', self.onKeypress)
+
+        # 告訴程式資料來自攝像頭， 還有鏡面效果
+        self._captureManager = CaptureManager(cv2.VideoCapture(0), self._windowManager, True)
+
+    def run(self):
+        """Run the main loop."""
+        self._windowManager.createWindow()
+        while self._windowManager.isWindowCreated:
+            # 這裡的enterFrame就是告訴程式從攝像頭中取資料
+            self._captureManager.enterFrame()
+
+            # 下面的這個frame是原始幀資料，這裡沒有做任何修改，後面的教程會對這個幀資料進行修改
+            frame = self._captureManager.frame
+
+            # exitFrame看起來是像是退出的意思，其實主要功能都是在這裡方法裡實現的，截圖、錄影都是在這裡
+            self._captureManager.exitFrame()
+
+            # 回撥函式
+            self._windowManager.processEvents()
+
+    # 定義鍵盤的回撥函式，用於self._windowManager.processEvents()的呼叫
+    def onKeypress(self, keyCode):
+        """
+        快捷鍵設定：
+        當按下“空格”鍵的時候，會進行抓屏。
+        當按下‘tab’鍵的時候，就開始或者停止錄影。
+        當然相應的目錄也會生成圖片或者視訊檔案
+        :param keyCode:
+        :return:
+        """
+        s = datetime.datetime.now().strftime('%Y-%m-%d')
+
+        # space
+        if keyCode == 32:
+            # 截圖儲存的檔名字
+            self._captureManager.writeImage(s + '.png')
+        # tab
+        elif keyCode == 9:
+            if not self._captureManager.isWritingVideo:
+                # 告訴程式，錄影儲存的檔名字
+                self._captureManager.startWritingVideo(s + '_DateVideo.avi')
+            else:
+                self._captureManager.stopWritingVideo()
+        # escape
+        elif keyCode == 27:
+            self._windowManager.destroyWindow()
+
+
+if __name__ == "__main__":
+    Cameo().run()
--- opencv/managers.py
+++ opencv/managers.py
@@ -84,6 +84,75 @@ class CaptureManager(object):
     def isWritingVideo(self):
         return self._videoFilename is not None
 
+    def enterFrame(self):
+        """Capture the next frame, if any"""
+        if self._capture is not None:
+            self._enteredFrame = self._capture.grab()
+
+    def exitFrame(self):
+        """Draw to the window. Write to files. Release the frame"""
+        if self.frame is None:
+            self._enteredFrame = False
+            return
+
+        # 計算fps
+        if self._framesElapsed == 0:
+            self._startTime = time.time()
+        else:
+            timeElapsed = time.time() - self._startTime
+            self._fpsEstimate = self._framesElapsed / timeElapsed
+
+        self._framesElapsed += 1
+
+        # 顯示影象
+        if self.previewWindowManager is not None:
+            if self.shouldMirrorPreview:
+                # 向左/右方向翻轉陣列，翻轉影象
+                mirroredFrame = np.fliplr(self._frame).copy()
+                self.previewWindowManager.show(mirroredFrame)
+            else:
+                self.previewWindowManager.show(self._frame)
+
+        # 圖片檔案生成
+        if self.isWritingImage:
+            cv2.imwrite(self._imageFilename, self._frame)
+            self._imageFilename = None
+
+        # 錄影生成
+        self._writeVideoFrame()
+
+        self._frame = None
+        self._enteredFrame = False
+
+    def writeImage(self, filename):
+        """Wirte the next exited frame to an image file."""
+        self._imageFilename = filename
+
+    def startWritingVideo(self, filename, encodeing=cv2.VideoWriter_fourcc('I', '4', '2', '0')):
+        """Start wirting exited frames to a video file."""
+        self._videoFilename = filename
+        self._videoEncoding = encodeing
+
+    def stopWritingVideo(self):
+        """Stop writing eited frames to a video file."""
+        self._videoFilename = None
+        self._videoEncoding = None
+        self._videoWriter = None
+
+    def _writeVideoFrame(self):
+        if not self.isWritingVideo:
+            return
+        if self._videoWriter is None:
+            fps = self._capture.get(cv2.CAP_PROP_FPS)
+            if fps == 0.0:
+                if self._framesElapsed < 20:
+                    return
+                else:
+                    fps = self._fpsEstimate
+            size = (int(self._capture.get(cv2.CAP_PROP_FRAME_WIDTH)), int(self._capture.get(cv2.CAP_PROP_FRAME_HEIGHT)))
+            self._videoWriter = cv2.VideoWriter(self._videoFilename, self._videoEncoding, fps, size)
+        self._videoWriter.write(self._frame)
+
 
 if __name__ == '__main__':
     pass
